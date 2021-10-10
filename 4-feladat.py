ae8e35dcd1e1cfbf38cdfb4cf2c1aea8c6c52624
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-10-10 09:19:41 +0200

    4. feladat

--- 4-feladat.py
+++ 4-feladat.py
@@ -1,15 +1,6 @@
 persons = ["John", "Marissa", "Pete", "Dayton"]
 restaurants = ["Japanese", "American", "Mexican", "French"]
 
-person = []
-restaurant = []
-
-for p in (persons):
-  person.append(p)
-
-for q in (restaurants):
-  restaurant.append(q)
-
 for i in range(len(persons)):
-  for f in range(len(restaurants)):
-    print(f'{person[i]} eats {restaurant[f]}')
+    for f in range(len(restaurants)):
+        print(f'{persons[i]} eats {restaurants[f]}')
